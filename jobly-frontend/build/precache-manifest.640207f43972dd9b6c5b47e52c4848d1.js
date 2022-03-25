bcb82c4de2063af4bb4cab0ef2289a2585c2aed9
aleksandre jakobia <sandro@aleksandres-MBP.home>
2022-03-25 17:36:50 -0400

    add all

---
+++ jobly-frontend/build/precache-manifest.640207f43972dd9b6c5b47e52c4848d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72039ba250abaa7ce6d0fa61d83a6f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3bd19af7d82791b7db9",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "1d572416a5cf4caf0ac7",
+    "url": "/static/css/main.658eb3e8.chunk.css"
+  },
+  {
+    "revision": "c3bd19af7d82791b7db9",
+    "url": "/static/js/2.11de26c8.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.11de26c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d572416a5cf4caf0ac7",
+    "url": "/static/js/main.12169987.chunk.js"
+  },
+  {
+    "revision": "50b8b38e359d53208b9e",
+    "url": "/static/js/runtime-main.8a240d35.js"
+  }
+]);
